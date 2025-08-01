0d192ce1e170c65275c4ba6709ed30a66f71e00e
Satxm <satxm@outlook.com>
2025-08-02 03:00:23 +0800

    New Accessibility Upgrade AGP 8.12.0

--- app/src/main/java/com/limelight/KeyboardAccessibilityService.java
+++ app/src/main/java/com/limelight/KeyboardAccessibilityService.java
@@ -1,7 +1,6 @@
 package com.limelight;
 
 import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.AccessibilityServiceInfo;
 import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.Toast;
@@ -23,41 +22,22 @@ public class KeyboardAccessibilityService extends AccessibilityService {
         int action = event.getAction();
         int keyCode = event.getKeyCode();
         // Toast.makeText(getApplicationContext(),"scancode:"+event.getScanCode()+",code:"+event.getKeyCode(),Toast.LENGTH_LONG).show();
-        // 主要解决系统自带快捷键在pc端无法使用问题 home键 scancode=172 code- 3
         if (Game.instance != null && Game.instance.connected && !BLACKLIST_KEYS.contains(keyCode)) {
-
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                event = new KeyEvent(event.getDownTime(), event.getEventTime(), event.getAction(),
+                        KeyEvent.KEYCODE_ESCAPE, event.getRepeatCount(), event.getMetaState(), event.getDeviceId(),
+                        event.getScanCode(), event.getFlags(), event.getSource());
+            }
             if (action == KeyEvent.ACTION_DOWN) {
-                // Fix 小米平板esc键按钮映射错误 KEYCODE_BACK=4
-                if (event.getScanCode() == 1) {
-                    Game.instance.handleKeyDown(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ESCAPE));
-                    return true;
-                }
                 Game.instance.handleKeyDown(event);
                 return true;
             } else if (action == KeyEvent.ACTION_UP) {
-                // Fix 小米平板esc键按钮映射错误 KEYCODE_BACK=4
-                if (event.getScanCode() == 1) {
-                    Game.instance.handleKeyUp(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_ESCAPE));
-                    return true;
-                }
                 Game.instance.handleKeyUp(event);
                 return true;
             }
         }
 
         return super.onKeyEvent(event);
-    }
-
-    @Override
-    public void onServiceConnected() {
-        LimeLog.info("Keyboard service is connected");
-        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        info.packageNames = new String[] { BuildConfig.APPLICATION_ID };
-        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
-        info.notificationTimeout = 100;
-        info.flags = AccessibilityServiceInfo.FLAG_REQUEST_FILTER_KEY_EVENTS;
-        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
-        setServiceInfo(info);
     }
 
     @Override
